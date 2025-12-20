eb0bba382af81228b25524d13e922b71011ae067
aqsmith02 <aqsmith02@gmail.com>
2025-12-20 02:33:40 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
-  base: '/madisons-game/'
+  base: '/madisons-game/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
 });
